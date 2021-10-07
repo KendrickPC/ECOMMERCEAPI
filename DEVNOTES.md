32f6240faef6eb1839b5918e671153887190261a
kendrickPC <kennethpchang@gmail.com>
2021-10-07 13:28:07 -0700

    auth login connected with database setup

---
+++ DEVNOTES.md
@@ -0,0 +1,7 @@
+Stopping Point: 48:09
+Postman
+
+https://www.youtube.com/watch?v=rMiRZ1iRC0A&ab_channel=LamaDev
+
+https://cryptojs.gitbook.io/docs/
+
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello Mars!")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB CONNECTION SUCCESSFUL (KINDA)"))
+  .catch((err) => {
+    console.log(err)
+  });
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is up and running! =) ")
+});
+
